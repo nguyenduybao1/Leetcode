320cc4eb41ad1b90b4fe7d4af01598648f2b2e09
Bảo Nguyễn <100182329+nguyenduybao1@users.noreply.github.com>
2026-03-22 19:10:25 +0700

    Time: 0 ms (100%), Space: 15.7 MB (33.26%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,20 +1,3 @@
-/*
-Leetcode 31 - Next Permutation
-Pattern: Greedy - TwoPointers
-
-Idea:
-- Traverse from right to left to find first index i where nums[i] < nums[i+1].
-  -> This is the pivot (first decreasing point).
-- From right to left, find first number > nums[i], swap them.
-- Reverse the suffix (i+1 → end) to get the smallest order.
-
-Key:
-- Right side is always decreasing.
-- Swap + reverse makes the next lexicographical permutation.
-
-Time Complexity: O(n)
-Space Complexity: O(1)
-*/
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
