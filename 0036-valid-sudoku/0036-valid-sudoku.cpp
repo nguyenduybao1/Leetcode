5958383d2eb0f86e3406d9a01933d5110767c993
Bảo Nguyễn <100182329+nguyenduybao1@users.noreply.github.com>
2026-08-11 21:52:53 +0700

    Time: 17 ms (5.08%), Space: 25.5 MB (20.5%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i = 0; i < 9; i++){
-            unordered_set<char> row;
-            unordered_set<char> col;
-            unordered_set<char> box;
+        vector<unordered_set<char>> rows(9);
+        vector<unordered_set<char>> cols(9);
+        vector<unordered_set<char>> boxes(9);
 
-            for(int j = 0; j < 9; j++){
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                char value = board[i][j];
 
-                if(board[i][j] != '.'){
-                    if(row.count(board[i][j])){
-                        return false;
-                    }
-                    row.insert(board[i][j]);
+                if(value == '.')
+                {
+                    continue;
                 }
 
-                if(board[j][i] != '.'){
-                    if(col.count(board[j][i])){
-                        return false;
-                    }
-                    col.insert(board[j][i]);
-                }
+                int boxId = (i / 3) * 3 + (j / 3);
 
-                int r = 3 * (i/3) + j/3;
-                int c = 3 * (i%3) + j%3;
-                if(board[r][c] != '.'){
-                    if(box.count(board[r][c])){
-                        return false;
-                    }
-                    box.insert(board[r][c]);
+                if(rows[i].count(value))
+                {
+                    return false;
                 }
+                rows[i].insert(value);
+
+                if(cols[j].count(value))
+                {
+                    return false;
+                }
+                cols[j].insert(value);
+
+                if(boxes[boxId].count(value))
+                {
+                    return false;
+                }
+                boxes[boxId].insert(value);
             }
         }
         return true;
